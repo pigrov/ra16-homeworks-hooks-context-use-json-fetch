01487bb136aae8f957e5771e1aefff5d7afb0c23
Dmitry Pigrov <pigrov@gmail.com>
2022-11-02 23:23:40 +0300

    final

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { useEffect, useState } from "react";
 
 const Data = () => {
   const [data, loading, error] = useJsonFetch("http://localhost:7070/data");
-  console.log(error);
+  console.log("data", data, "loading", loading, "error", error);
 
   return (
     <div className="Data">
       <div className="Result">
-        {loading && "Loading..."}
-        {data.status}
-        {error && <span>Error </span>}
+        <p>1. {loading && "Loading..."}</p>
+        <p>2. {data.status}</p>
+        <p>3. {error && <span>Error </span>}</p>
       </div>
     </div>
   );
@@ -26,11 +26,13 @@ function useJsonFetch(url, opts) {
       try {
         setLoading(true);
         const response = await fetch(url, opts);
+        console.log(response);
+
         if (response.ok) {
           const data = await response.json();
-          setError(response.status + " " + data.status);
+          console.log(data);
+          setData(data);
         }
-        setData(data);
         setLoading(false);
       } catch (e) {
         setError(e);
