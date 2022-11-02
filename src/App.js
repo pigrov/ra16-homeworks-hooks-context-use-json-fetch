2571898e6f18ede24862366100b5279aa14a3112
Dmitry Pigrov <pigrov@gmail.com>
2022-11-02 23:03:38 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const Data = () => {
+  const [data, loading, error] = useJsonFetch("http://localhost:7070/data");
+  console.log(error);
+
+  return (
+    <div className="Data">
+      <div className="Result">
+        {loading && "Loading..."}
+        {data.status}
+        {error && <span>Error </span>}
+      </div>
+    </div>
+  );
+};
+
+function useJsonFetch(url, opts) {
+  const [data, setData] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setData("");
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(url, opts);
+        if (response.ok) {
+          const data = await response.json();
+          setError(response.status + " " + data.status);
+        }
+        setData(data);
+        setLoading(false);
+      } catch (e) {
+        setError(e);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return [data, loading, error];
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Data />
     </div>
   );
 }
